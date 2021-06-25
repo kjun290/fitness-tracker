8a1bc607ad9f189306eba6728fca73698731b371
Kevin Jun <kwjun90@gmail.com>
2021-06-25 09:23:59 -0500

    added server.js

--- Develop/models/workout.js
+++ Develop/models/workout.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const workoutSchema = new mongoose.Schema(
+const workoutSchema = new Schema(
     {
         day: {
         type: Date,
