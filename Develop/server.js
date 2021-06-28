0cca544f688d0111ea5d10e156c17f145e86a70e
Kevin Jun <kwjun90@gmail.com>
2021-06-28 16:29:39 -0500

    completed routes

--- Develop/server.js
+++ Develop/server.js
@@ -14,17 +14,10 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/userdb", { useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
 
-app.post("/submit", ({ body }, res) => {
-  model.create(body)
-    .then(dbUser => {
-      res.json(dbUser);
-    })
-    .catch(err => {
-      res.json(err);
-    });
-});
+app.use(require("./routes/api.js"));
+app.use(require("./routes/view.js"));
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
